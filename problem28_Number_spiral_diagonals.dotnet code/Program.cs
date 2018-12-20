509b57c26ce1a7827a8a0dbab7db36242eecdd9a
Shlomi.O <shlomio@codevalue.net>
2018-12-20 12:43:52 +0200

    first commit

---
+++ problem28_Number_spiral_diagonals.dotnet code/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace problem28_Number_spiral_diagonals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = GenerateSpiralMatrix(1001);
+            IEnumerable<int[]> diagonals = GetDiagonals(matrix);
+            var diagonalsSum = diagonals.Sum(d=>d[0]);
+            Console.WriteLine($"Diagonals Sum = {diagonalsSum}");
+        }
+
+        private static IEnumerable<int[]> GetDiagonals(int[,] matrix)
+        {
+            var matrixLength = matrix.GetLength(0);
+            var diagonals = new List<int[]>();
+            diagonals.Add(new int[matrixLength]);
+            diagonals.Add(new int[matrixLength]);
+            return diagonals;
+        }
+
+        private static int[,] GenerateSpiralMatrix(int size)
+        {
+            var spiralMatrix = new int[size,size];
+            return spiralMatrix;
+        }
+    }
+}
---
+++ problem28_Number_spiral_diagonals/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace problem28_Number_spiral_diagonals
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            int[,] matrix = GenerateSpiralMatrix(1001);
+            IEnumerable<int[]> diagonals = GetDiagonals(matrix);
+            var diagonalsSum = diagonals.SelectMany(d => d).Sum();
+            Console.WriteLine($"Diagonals Sum = {diagonalsSum}");
+        }
+
+        private static IEnumerable<int[]> GetDiagonals(int[,] matrix)
+        {
+            var matrixLength = matrix.GetLength(0);
+            var diagonals = new List<int[]>
+            {
+                new int[matrixLength],
+                new int[matrixLength]
+            };
+
+            var idx = 10;
+            for (int i0 = idx; i0 < 12; i0++)
+            for (int i1 = 0; i1 < matrixLength; i1++)
+                diagonals[i0-idx][i1] = i0 * i1;
+            
+            return diagonals;
+        }
+
+        private static int[,] GenerateSpiralMatrix(int size)
+        {
+            var spiralMatrix = new int[size, size];
+
+            var length0 = spiralMatrix.GetLength(0);
+            var length1 = spiralMatrix.GetLength(1);
+
+            for (int i0 = 0; i0 < length0; i0++)
+            for (int i1 = 0; i1 < length1; i1++)
+                spiralMatrix[i0, i1] = i0 * i1;
+
+            return spiralMatrix;
+        }
+    }
+}
